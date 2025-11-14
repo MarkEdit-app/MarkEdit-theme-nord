1664a10f63da160a2940b9ae7f14a652896322c0
Ying Zhong <0x00eeee@gmail.com>
2025-11-14 08:13:46 +0800

    Improve active line background color readability

--- main.ts
+++ main.ts
@@ -4,5 +4,10 @@ import { nord } from 'cm6-theme-nord';
 overrideThemes({
   dark: {
     extension: nord,
+    colors: {
+      editor: {
+        activeLineBackground: '#4c566a66',
+      },
+    },
   },
 });
